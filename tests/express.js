81d31d0c2ce2c302aaa4423a578fd5d02d31982b
Finn <finnterdal@gmail.com>
2020-01-31 00:51:50 -0600

    prepare for lecture

---
+++ tests/express.js
@@ -0,0 +1,6 @@
+/* eslint-env mocha */
+import { expect } from "chai"
+
+describe("Express", () => {
+  it("returns some data", () => {})
+})
---
+++ tests/react.js
@@ -0,0 +1,8 @@
+/* eslint-env mocha */
+import React from "react"
+import { expect } from "chai"
+import { mount, shallow } from "enzyme"
+
+describe("React", () => {
+  it("renders some things", () => {})
+})
